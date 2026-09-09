3a5b0afd364a6dd15630c600f7d930816bda35b9
CrusaderNoen <160173544+CrusaderNoen@users.noreply.github.com>
2026-09-09 07:30:03 -0400

    Add files via upload

--- Homework1/Project#1/Zpm.java
+++ Homework1/Project#1/Zpm.java
@@ -1,86 +1,111 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.HashMap;
-import java.util.InputMismatchException;
 
 public class Zpm {  
     
-    public static void main(String args[]) {        
-        Scanner scannerrObject = new Scanner(args[0]);
+    public static void main(String[] args)throws FileNotFoundException {        
+        Scanner scannerObject = new Scanner(new File(args[0]));
+        //Scanner scannerObject = new Scanner(new File("prog3.zpm"));
         HashMap<String, String> objects = new HashMap<>();
-        int currentLine = 0;        
+        int currentLine = 0;
         
-        while (scannerrObject.hasNextLine()) {            
-          currentLine++;
-          String data = scannerrObject.nextLine();
-          StringTokenizer tokenizer = new StringTokenizer(data, " ()");     
+        while (scannerObject.hasNextLine()) {            
+            currentLine++;
+            //System.out.println(currentLine);
+            String data = scannerObject.nextLine();
+            StringTokenizer tokenizer = new StringTokenizer(data, " ()");     
           
-          while (tokenizer.hasMoreTokens()) {                
-              String token = tokenizer.nextToken();
+                        
+            String tempVar = tokenizer.nextToken();
               
-              if(token.equals("PRINT")) { //Code for printing out a value (Done)                  
-                  try {                      
-                      System.out.println(objects.get(tokenizer.nextToken()));                      
-                  }catch(InputMismatchException er) {                      
-                      System.out.println("RUNTIME ERROR: line " + currentLine);  
-                  }
-              }else if(token.equals("FOR")) { //Code for for loops
-                  
-              }else {                  
-                  String tempVar = token;
-                  
-                  if (objects.containsKey(token)) {                     
-                      String tempArg = tokenizer.nextToken();
-                      String tempCompliment = tokenizer.nextToken();
-                      
-                      if (tempArg.equals("+=")) { //Code for += (done)                          
-                          if(canBeInt(objects.get(tempVar)) && canBeInt(objects.get(tempCompliment))) {                             
-                              objects.put(tempVar, Integer.toString(Integer.parseInt(objects.get(tempVar)) + Integer.parseInt(objects.get(tempCompliment))));                             
-                          } else {                              
-                              objects.put(tempVar, objects.get(tempVar) + objects.get(tempCompliment));                              
-                          }                                       
-                      } else if (tempArg.equals("*=")) { //Code for *= (done)                          
-                          if(canBeInt(objects.get(tempVar)) && canBeInt(objects.get(tempCompliment))) {                              
-                              objects.put(tempVar, Integer.toString(Integer.parseInt(objects.get(tempVar)) * Integer.parseInt(objects.get(tempCompliment))));                              
-                          } else {                              
-                              scannerrObject.close();
-                              throw new IllegalArgumentException("ERROR: cannot multiply Strings on line " + currentLine);                              
-                          }
-                      } else if (tempArg.equals("-=")) { //Code for -= (done)                          
-                          if(canBeInt(objects.get(tempVar)) && canBeInt(objects.get(tempCompliment))) {                              
-                              objects.put(tempVar, Integer.toString(Integer.parseInt(objects.get(tempVar)) - Integer.parseInt(objects.get(tempCompliment))));                              
-                          } else {                              
-                              scannerrObject.close();
-                              throw new IllegalArgumentException("ERROR: cannot subtract Strings on line " + currentLine);                              
-                          }    
-                      } else {
-                          scannerrObject.close();
-                          throw new IllegalArgumentException("ERROR: illegal argument on line " + currentLine);
-                      }
-                  } else {                      
-                      if(tokenizer.nextToken().equals("=")) { //Code for adding values (Done)                          
-                          String tempCompliment = tokenizer.nextToken();                         
-                          if (objects.containsKey(tempCompliment)) {                              
-                              objects.put(tempVar, objects.get(tempCompliment));                              
-                          } else {                              
-                              objects.put(tempVar, tempCompliment);                              
-                          }
-                      } else {                          
-                          scannerrObject.close();
-                          throw new IllegalArgumentException("ERROR: " + tempVar + "does not exist on line " + currentLine);
-                      }
-                  }
-              }
-          }
-          if(!tokenizer.nextToken().equals(";")) { //Code to check for semicolons
-              scannerrObject.close();
-              throw new IllegalArgumentException("ERROR: line " + currentLine + " does not end in a semicolon");
-          }
-        }        
+            if (tempVar.equals("FOR")) { //Code for for loops
+                int loopCount = Integer.parseInt(tokenizer.nextToken());
+                for (int i = 0; i < loopCount; i++) {
+                    StringTokenizer tempizer = new StringTokenizer(data, " ()");
+                    tempizer.nextToken(); 
+                    tempizer.nextToken();
+                    String newToken = tempizer.nextToken();
+                    boolean exit = false;
+                    while (!exit) {
+                        zpmArg(newToken, tempizer, scannerObject, 
+                                objects, currentLine);
+                        tempizer.nextToken();
+                        newToken = tempizer.nextToken();
+                        if (newToken.equals("ENDFOR")) {
+                            exit = true;
+                        }
+                    }
+                }
+            } else {                                                    
+                zpmArg(tempVar, tokenizer, scannerObject, objects, currentLine);
+            }
+        }
+                
         //Closing the file
-        scannerrObject.close();
+        scannerObject.close();
     }   
+    
+    //helper method for printing 
+    public static void zpmPrint(String val, HashMap<String, String> objects, int currentLine) {             
+        System.out.println(objects.get(val));                      
+    }
+    
+    //method for performing arguments
+    public static void zpmArg(String tempVar, StringTokenizer tokenizer, Scanner scannerObject, HashMap<String, String> objects, int currentLine) {
+        if (objects.containsKey(tempVar)) {                     
+            String tempArg = tokenizer.nextToken();
+            if (tempArg.equals("+=")) { //Code for += (done)  
+                String tempCompliment = tokenizer.nextToken();
+                if (tempCompliment.substring(0, 1).equals("\"")) {
+                    objects.put(tempVar, objects.get(tempVar) + getFullString(tokenizer, tempCompliment));
+                } else if(canBeInt(tempCompliment)) {
+                    objects.put(tempVar, Integer.toString(Integer.parseInt(objects.get(tempVar)) + Integer.parseInt(tempCompliment)));
+                }else if (objects.containsKey(tempCompliment)) {
+                    objects.put(tempVar, objects.get(tempVar)+ objects.get(tempCompliment));
+                } else {
+                    scannerObject.close();
+                    throw new IllegalArgumentException("ERROR: " + tempCompliment + " does not exist on line " + currentLine);
+                }        
+            } else if (tempArg.equals("*=")) { //Code for *= (done)  
+                String tempCompliment = tokenizer.nextToken();
+                if(canBeInt(tempCompliment)) {
+                    objects.put(tempVar, Integer.toString(Integer.parseInt(objects.get(tempVar)) * Integer.parseInt(tempCompliment)));
+                }else if (objects.containsKey(tempCompliment)) {
+                    objects.put(tempVar, Integer.toString(Integer.parseInt(objects.get(tempVar)) * Integer.parseInt(objects.get(tempCompliment))));
+                } else {
+                    scannerObject.close();
+                    throw new IllegalArgumentException("ERROR: " + tempCompliment + " does not exist on line " + currentLine);
+                }
+            } else if (tempArg.equals("-=")) { //Code for -= (done)  
+                String tempCompliment = tokenizer.nextToken();
+                if(canBeInt(tempCompliment)) {
+                    objects.put(tempVar, Integer.toString(Integer.parseInt(objects.get(tempVar)) - Integer.parseInt(tempCompliment)));
+                }else if (objects.containsKey(tempCompliment)) {
+                    objects.put(tempVar, Integer.toString(Integer.parseInt(objects.get(tempVar)) - Integer.parseInt(objects.get(tempCompliment))));
+                } else {
+                    scannerObject.close();
+                    throw new IllegalArgumentException("ERROR: " + tempCompliment + " does not exist on line " + currentLine);
+                }           
+            }
+        } else if (tempVar.equals("PRINT")) {
+            zpmPrint(tokenizer.nextToken(), objects, currentLine);
+        } else {                      
+            if (tokenizer.nextToken().equals("=")) { //Code for adding values (Done)                          
+                String tempCompliment = getFullString(tokenizer, tokenizer.nextToken());
+                if (objects.containsKey(tempCompliment)) {                              
+                    objects.put(tempVar, objects.get(tempCompliment));                              
+                } else {                              
+                    objects.put(tempVar, tempCompliment);                              
+                }
+            } else {                          
+                scannerObject.close();
+                throw new IllegalArgumentException("ERROR: " + tempVar + " does not exist on line " + currentLine);
+            }
+        }
+    }
     
     
     public static boolean canBeInt(String str) { //helper method for checking if a value is an int
@@ -90,5 +115,27 @@ public class Zpm {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+    
+    public static String getFullString(StringTokenizer tokenizer, String temp) { //helper method for getting a full string
+        String tempCompliment = temp;
+        if (tempCompliment.startsWith("\"")) {
+            while (!tempCompliment.endsWith("\"")) {
+                tempCompliment += " " + tokenizer.nextToken();
+            }
+            tempCompliment = tempCompliment.substring(1, tempCompliment.length() - 1);
+        }
+        return tempCompliment;
+    }
+    
+    public static String parseQuotes(String str) {
+        String temp = str;
+        if (str.substring(0, 1).equals("\"")) {
+            temp = temp.substring(1);
+        }
+        if (str.substring(str.length() - 1).equals("\"")) {
+            temp = temp.substring(0, str.length() - 2);
+        }
+        return temp;
     }
 }
